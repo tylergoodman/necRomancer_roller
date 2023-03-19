00e433ad990527b92c90892375ae034c1b54f36d
Tyler Goodman <tylergoodman@users.noreply.github.com>
2023-03-19 03:36:20 +0000

    vite test initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/necRomancer_roller/",
+  build: {
+    lib: {
+      entry: "src/my-element.ts",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: /^lit/,
+    },
+  },
+});
