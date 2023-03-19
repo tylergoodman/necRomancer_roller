defedb19244ac3a9af27daed086c972f036f2587
Tyler Goodman <tylergoodman@users.noreply.github.com>
2023-03-19 03:56:37 +0000

    vite template doesnt include lit because its in library mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/necRomancer_roller/",
-  build: {
-    rollupOptions: {
-      external: /^lit/,
-    },
-  },
 });
